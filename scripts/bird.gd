7c44886157300f9472bea8bf0a35a9702e42055f
sandy-5000 <sandeepkumarbhaviri@gmail.com>
2024-07-26 09:23:56 +0530

    init

---
+++ scripts/bird.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+const GRAVITY = 1000
+const MAX_VEL = 600
+const FLAP_SPEED = -500
+var flying = false
+var falling = false
+#const START_POS = Vector2(243, 432)
+const START_POS = Vector2(0, 0)
+
+func _ready():
+	reset()
+
+func reset():
+	falling = false
+	flying = false
+	position = START_POS
+	rotation = deg_to_rad(0)
+
+
+func _physics_process(delta):
+	if not flying and not falling:
+		$AnimatedSprite2D.stop()
+		return
+	velocity.y += GRAVITY * delta
+	if velocity.y > MAX_VEL:
+		velocity.y = MAX_VEL
+	if flying:
+		rotation = deg_to_rad(velocity.y * 0.05)
+		$AnimatedSprite2D.play("flying")
+	elif falling:
+		rotation = PI / 2
+		$AnimatedSprite2D.stop()
+	move_and_collide(velocity * delta)
+
+func flap():
+	velocity.y = FLAP_SPEED
+
---
+++ scripts/main.gd
@@ -0,0 +1,20 @@
+extends Node
+
+var game_running = false
+var game_over = false
+var scroll
+const SCROLL_SPEED = 4
+var screen_size: Vector2i
+var ground_height: int
+var pipes: Array
+const PIPE_DELAY = 100
+const PIPE_RANGE = 200
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
